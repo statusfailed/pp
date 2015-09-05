0cf34880099e688b7b74382bc4b6f2f9a7920590
status failed <sf@statusfailed.com>
2015-09-05 22:19:05 +0000

    added main test file

---
+++ Main.hs
@@ -0,0 +1,13 @@
+module Main where
+
+import Control.Monad.Free
+import Language.PP
+import Language.PP.Dist
+import Language.PP.Eval
+import Language.PP.Infer
+import Data.Random (MonadRandom)
+
+modelPmmh :: MonadRandom m => PMMH m Double [S]
+modelPmmh = (liftF (uniform 0 1), (\x y -> 1), model)
+
+main = eval (pmmh 100 100 modelPmmh) >>= mapM_ print
