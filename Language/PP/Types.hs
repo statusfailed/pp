51f7b7405a240f9defba0de02f0a7e5be3107e8b
status failed <sf@statusfailed.com>
2015-12-13 23:39:41 +0000

    add some comments to Types.hs, add executable to pp.cabal, update shell.nix and +x reconfig.sh

--- Language/PP/Types.hs
+++ Language/PP/Types.hs
@@ -9,9 +9,15 @@ import Control.Monad.Free
 type Probability = Double
 type LogP        = Double
 
+-- | A 'P m a' is a probabilistic value a, which uses a monad m as a source of
+-- randomness.  it can be run (with 'runP' to produce a value and a
+-- "Probability" of that value. 
+-- NOTE: the "Probability" is not truly a probability, so won't sum to 1.
 data P m a = P { runP :: m (Probability, a) }
 
 instance Monad m => Functor (P m) where
   fmap f (P m) = P $ fmap (fmap f) m -- 2nd fmap is tuples
 
+-- | A Probabilistic Program is just the combination of many probabilistic
+-- values using Free.
 type PP m = Free (P m)
--- shell.nix
+++ shell.nix
@@ -4,18 +4,27 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, base, free, random-fu, stdenv }:
+  f = { mkDerivation, base, containers, free, machines, random-fu
+      , stdenv
+      }:
       mkDerivation {
         pname = "pp";
         version = "0.1.0.0";
         src = /home/sf/projects/academic/pp;
-        libraryHaskellDepends = [ base free random-fu ];
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          base containers free machines random-fu
+        ];
+        executableHaskellDepends = [
+          base containers free machines random-fu
+        ];
         license = stdenv.lib.licenses.mit;
       };
 
   haskellPackages = if compiler == "default"
-                      then pkgs.haskellPackages
-                      else pkgs.haskell.packages.${compiler};
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
 
   drv = haskellPackages.callPackage f {};
 
